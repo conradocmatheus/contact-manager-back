603ba9208e58651b67a63393fbd64b85be640ab7
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-07 18:13:36 -0300

    wip: more tests, will add ORM now

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/test", (req, res) => {
-    res.send("first test");
+const contacts = [];
+
+app.use(express.json());
+
+app.get("/contacts", (req, res) => {
+    res.status(200).json(contacts);
 });
 
-app.listen(3000);
+app.post("/contacts", (req, res) => {
+    contacts.push(req.body);
+    res.status(201).send(req.body);
+})
+
+app.listen(port, () => console.log(`Server now running on port ${port}`));
