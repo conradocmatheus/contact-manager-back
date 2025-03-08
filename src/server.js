555a4f3fc8c274eb0e879f6bea52cca743337b12
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 00:48:58 -0300

    feat: using errorHandler in server.js now

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from './utils/middlewares/errorHandler.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRoutes)
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
