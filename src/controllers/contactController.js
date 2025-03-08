cbf3ed33863a5ee97ce16e93d86c985b46c5ca3f
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 02:21:57 -0300

    feat: createcontact method

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -5,3 +5,21 @@ export const getAllContacts = asyncHandler(async (req, res) => {
     const contacts = await prisma.contact.findMany();
     res.status(200).json(contacts);
 });
+
+export const createContact = asyncHandler(async (req, res) => {
+    const { name, email, phone, userId } = req.body;
+
+    const existingContact = await prisma.contact.findUnique({
+        where: { email }
+    });
+
+    if (existingContact) {
+        return res.status(400).json({ error: 'Email already in use' });
+    }
+
+    const contact = await prisma.contact.create({
+        data: { name, email, phone, userId }
+    });
+
+    res.status(201).json(contact);
+});
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import {getAllContacts} from '../controllers/contactController.js';
+import {createContact, getAllContacts} from '../controllers/contactController.js';
 
 const router = new Router();
 
 router.get('/', getAllContacts);
+router.post('/', createContact);
