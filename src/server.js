db986e40ff0b2211988f818ee3e3543d6925509c
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 02:15:55 -0300

    style: changed starting message

--- src/server.js
+++ src/server.js
@@ -13,5 +13,5 @@ app.use(errorHandler);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Now listening on port ${port}`);
 });
