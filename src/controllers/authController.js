c4e8f91fd13577fb46aef74e08bda4210b92ba93
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-10 14:23:30 -0300

    feat: adapted methods to work with the asyncHandler middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
+import { asyncHandler } from '../utils/middlewares/asyncHandler.js';
 
 const prisma = new PrismaClient();
 
@@ -9,52 +10,40 @@ const generateToken = (userId) => {
     return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
 
-export const signup = async (req, res) => {
+export const signup = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
-    try {
-        const userExists = await prisma.user.findUnique({ where: { email } });
-        if (userExists) {
-            return res.status(400).json({ message: 'Usuário já existe' });
-        }
-
-        const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-        const user = await prisma.user.create({
-            data: { email, password: hashedPassword, name },
-        });
-
-        const token = generateToken(user.id);
-
-        res.status(201).json({ token, user: { id: user.id, email: user.email, name: user.name } });
-    } catch (error) {
-        res.status(500).json({ message: 'Ocorreu um erro no servidor' });
-    } finally {
-        await prisma.$disconnect();
+    const userExists = await prisma.user.findUnique({ where: { email } });
+    if (userExists) {
+        return res.status(400).json({ message: 'Usuário já existe' });
     }
-};
 
-export const login = async (req, res) => {
+    const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await prisma.user.create({
+        data: { email, password: hashedPassword, name },
+    });
+
+    const token = generateToken(user.id);
+
+    res.status(201).json({ token, user: { id: user.id, email: user.email, name: user.name } });
+});
+
+export const login = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-    try {
-        const user = await prisma.user.findUnique({ where: { email } });
-        if (!user) {
-            return res.status(400).json({ message: 'Credenciais inválidas' });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Credenciais inválidas' });
-        }
-
-        const token = generateToken(user.id);
-
-        res.status(200).json({ token, user: { id: user.id, email: user.email, name: user.name } });
-    } catch (error) {
-        res.status(500).json({ message: 'Ocorreu um erro no servidor' });
-    } finally {
-        await prisma.$disconnect();
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+        return res.status(400).json({ message: 'Credenciais inválidas' });
     }
-};
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Credenciais inválidas' });
+    }
+
+    const token = generateToken(user.id);
+
+    res.status(200).json({ token, user: { id: user.id, email, name: user.name } });
+});
