8ab19611fa4b16f25cf257c40393c5b1262ecc0b
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-12 17:43:06 -0300

    feat: phone validation works

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
 import { errorHandler } from './utils/middlewares/errorHandler.js';
 import authRoutes from "./routes/authRoutes.js";
+import axios from "axios";
 
 const app = express();
 
@@ -17,6 +18,23 @@ app.use('/auth', authRoutes);
 app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
+
+app.get('/validate-phone', async (req, res) => {
+    const { number } = req.query;
+
+    try {
+        const response = await axios.get(process.env.NUMVERIFY_API_URL, {
+            params: {
+                access_key: process.env.NUMVERIFY_API_KEY,
+                number: number,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erro ao validar o número:', error);
+        res.status(500).send('Erro ao validar o número');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
